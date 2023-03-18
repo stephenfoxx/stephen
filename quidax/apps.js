46f2c2f48088c74e4469587f04d4720dca879767
stephenfoxx <116175711+stephenfoxx@users.noreply.github.com>
2023-03-18 02:42:19 -0700

    Add files via upload
    a project built using html, bootstrap and javascript

---
+++ quidax/apps.js
@@ -0,0 +1,38 @@
+// $(document).ready(function() {
+//     $('.btn').click(function () {
+//         //  $('.box').hide("slow")
+//         // $('.box').show("slow")
+//         // $('.box').toggle("slow")
+//         // $('.box').slideToggle(5000);
+//         // $('.box').slideUp(5000);
+//         // $('.box').slideDown(5000);
+
+//         // $(".box").css({ backgroundColor: "purple", opacity:0.5})
+//         // alert('hello jquery')
+//         // prompt
+//         // confirm
+
+
+//         $(".box")
+//             .animate({ width: "300px", left: "500px"})
+//             .animate({ height: "300px", top: "500px"})
+//             .animate({ width: "200px", left: "0"})
+//             .animate({ height: "200px", top: "0"})
+//     })
+// })
+
+// // other types of events
+// //     .dbl click
+// // .hover
+
+$(document).ready(function () {
+  $(".fa-bars").click(function () {
+    $("nav ul").toggleClass("show");
+  });
+});
+
+
+$('.products').hover(function () {
+    
+})
+    
